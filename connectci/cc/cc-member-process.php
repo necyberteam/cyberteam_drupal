4e90605b621e8a82feeb26d7a3a4b56788b6bbc7
Andrew Pasquale <andrew@elytra.net>
2022-01-07 13:17:05 -0500

    Update user__field_is_cc and user__field_region tables for campus champions

--- connectci/cc/cc-member-process.php
+++ connectci/cc/cc-member-process.php
@@ -22,6 +22,36 @@ if (isset($sid) && $sid != "") {
   $updateApplication->execute();
   $updateApplication->close();
 }
+// Update user__field_is_cc table with approved status
+$bundle = 'user';
+$deleted = 0;
+$entity_id = $uid;
+$revision_id = $uid;
+$langcode = 'en';
+$delta = 0;
+$region_id = 572; // Campus Champions Region
+$updateCC = $conn->prepare("INSERT INTO user__field_is_cc (bundle, deleted, entity_id, revision_id, langcode, delta, field_is_cc_value) VALUES (?,?,?,?,?,?,?) ON DUPLICATE KEY UPDATE field_is_cc_value = ?");
+$updateCC->bind_param("sisisiss", $bundle, $deleted, $entity_id, $revision_id, $langcode, $delta, $approved, $approved);
+$updateCC->execute();
+$updateCC->close();
+// Update user__field_regions table
+if ($approved) {
+  // Set delta to one more than the current highest number
+  $sql = 'select MAX(delta) from user__field_region ufr WHERE ufr.entity_id='.$entity_id;
+  $result = $conn->query($sql);
+  if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+      $delta = $row['MAX(delta)'] + 1;
+    }
+  }
+  $updateCC = $conn->prepare("INSERT INTO user__field_region (bundle, deleted, entity_id, revision_id, langcode, delta, field_region_target_id) VALUES (?,?,?,?,?,?,?)");
+  $updateCC->bind_param("sisisii", $bundle, $deleted, $entity_id, $revision_id, $langcode, $delta, $region_id);
+} else {
+  $updateCC = $conn->prepare("DELETE FROM user__field_region ufr WHERE ufr.entity_id = ? AND ufr.field_region_target_id = ?");
+  $updateCC->bind_param("ss", $entity_id, $region_id);
+}
+$updateCC->execute();
+$updateCC->close();
 if ($_POST['type'] == "new") {
   //insert new record
   $insertCC = $conn->prepare("INSERT INTO campus_champions(uid, carnegie_code, approved, classification) VALUES (?,?,?,?)");
