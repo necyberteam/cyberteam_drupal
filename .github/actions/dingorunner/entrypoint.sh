75b711ad779e8a71a80c2ef2fcf11ea466a8b541
Miles France <miles.france@protitude.com>
2022-07-07 16:22:07 +0000

    github: switch back to master branch

--- .github/actions/dingorunner/entrypoint.sh
+++ .github/actions/dingorunner/entrypoint.sh
@@ -28,5 +28,5 @@ then
   storeKey
   sh -c "composer config -g github-oauth.github.com $gh_token"
   composer install --no-dev --ignore-platform-reqs
-  GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" $blt artifact:deploy --commit-msg "$message" --branch "main" --ignore-dirty --ignore-platform-reqs --no-interaction --verbose
+  GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" $blt artifact:deploy --commit-msg "$message" --branch "master" --ignore-dirty --ignore-platform-reqs --no-interaction --verbose
 fi
