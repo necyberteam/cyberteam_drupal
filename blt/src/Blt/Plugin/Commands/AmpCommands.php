15fad21436e09052523ff969ebb9dcfd3d05da50
protitude <miles.france@gmail.com>
2022-08-30 18:17:02 +0000

    copy general behat tests to other sites and add correct @tag

--- blt/src/Blt/Plugin/Commands/AmpCommands.php
+++ blt/src/Blt/Plugin/Commands/AmpCommands.php
@@ -69,6 +69,9 @@ AMP_USERNAME=$username'>.env");
     $lando_end = $this->lando() == 'lando '?"\"":"";
 
     foreach ($domains as $domain) {
+      if ($domain != 'cyberteam') {
+        $behat = shell_exec("cp tests/behat/features/cyberteam/* tests/behat/features/$domain/ && sed -i '1 s/@cyberteam/@$domain/g' tests/behat/features/$domain/*.feature");
+      }
       $behat = shell_exec($lando . '\'google-chrome\' --headless --no-sandbox --disable-dev-shm-usage --disable-web-security --remote-debugging-port=9222 &) | behat  --format pretty /app/tests/behat --colors --no-interaction --stop-on-failure --config /app/tests/behat/local.yml --profile local --tags @' . $domain . ' -v' . $lando_end);
       $this->say($behat);
       $this->_exec( $this->lando() . 'drush cim -y');
