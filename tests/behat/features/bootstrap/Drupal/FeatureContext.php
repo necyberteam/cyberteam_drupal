f1e99b13071ab43f334ff1821a1af51c062d9cb8
Andrew Pasquale <andrew@elytra.net>
2022-09-23 14:03:50 -0400

    Behat test for disabled button

--- tests/behat/features/bootstrap/Drupal/FeatureContext.php
+++ tests/behat/features/bootstrap/Drupal/FeatureContext.php
@@ -2,48 +2,66 @@
 
 namespace Drupal;
 
+use Behat\Behat\Tester\Exception\PendingException;
 use Drupal\DrupalExtension\Context\RawDrupalContext;
 
 /**
  * FeatureContext class defines custom step definitions for Behat.
  */
-class FeatureContext extends RawDrupalContext {
+class FeatureContext extends RawDrupalContext
+{
 
-  /**
-   * Every scenario gets its own context instance.
-   *
-   * You can also pass arbitrary arguments to the
-   * context constructor through behat.yml.
-   */
-  public function __construct() {
+    /**
+     * Every scenario gets its own context instance.
+     *
+     * You can also pass arbitrary arguments to the
+     * context constructor through behat.yml.
+     */
+    public function __construct()
+    {
 
-  }
-
-  /**
-   * Waits a while, for debugging.
-   *
-   * @param int $seconds
-   *   How long to wait.
-   *
-   * @When I wait :seconds second(s)
-   */
-  public function wait($seconds) {
-    sleep($seconds);
-  }
-
-  /**
-  * @Given I click the :arg1 element
-  */
-  public function iClickTheElement($selector)
-  {
-    $page = $this->getSession()->getPage();
-    $element = $page->find('css', $selector);
-
-    if (empty($element)) {
-        throw new Exception("No html element found for the selector ('$selector')");
     }
 
-    $element->click();
-  }
+    /**
+     * Waits a while, for debugging.
+     *
+     * @param int $seconds
+     *   How long to wait.
+     *
+     * @When I wait :seconds second(s)
+     */
+    public function wait($seconds)
+    {
+        sleep($seconds);
+    }
 
+    /**
+     * @Given I click the :arg1 element
+     */
+    public function iClickTheElement($selector)
+    {
+        $page = $this->getSession()->getPage();
+        $element = $page->find('css', $selector);
+
+        if (empty($element)) {
+            throw new Exception("No html element found for the selector ('$selector')");
+        }
+
+        $element->click();
+    }
+
+    /**
+     * @When I should see the :selector button is disabled 
+     */
+    public function iShouldSeetheButtonIsDisabled($selector)
+    {
+        $page = $this->getSession()->getPage();
+        $element = $page->find('css', $selector);
+        if (empty($element)) {
+            throw new PendingException("No html element found for the selector ('$selector')");
+        }
+        if (!$element->hasClass('disabled')) {
+            throw new PendingException("Element '$selector' is not disabled");
+        }
+    }
 }
