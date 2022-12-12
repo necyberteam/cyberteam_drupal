b4909efe70e4c4e0b4482e5962a94f26e1cc2864
protitude <miles.france@gmail.com>
2022-12-12 00:04:39 +0000

    codespaces: adding github-protocols to fix push url in custom modules #nobuild

--- .devcontainer/codespaces.sh
+++ .devcontainer/codespaces.sh
@@ -7,6 +7,7 @@ php -r "if (hash_file('sha384', 'composer-setup.php') === '55ce33d7678c5a6110855
 php composer-setup.php
 php -r "unlink('composer-setup.php');"
 sudo mv composer.phar /usr/local/bin/composer
+lando composer config --global github-protocols https
 
 mkdir -p ~/.lando/cache
 composer install --ignore-platform-reqs -n
