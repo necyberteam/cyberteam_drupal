21680f424f8049405e438388d39c3270c37e474b
protitude <miles.france@gmail.com>
2022-12-16 16:45:40 +0000

    codespaces: set php to 8.1 — install terminus — add terminus login if proper secrets are setup — fix amp_user to amp_uid — update readme

--- .devcontainer/codespaces.sh
+++ .devcontainer/codespaces.sh
@@ -10,8 +10,15 @@ sudo mv composer.phar /usr/local/bin/composer
 composer config --global github-protocols https
 lando composer config --global github-protocols https
 
+if [ -z ${AMP_TERMINUS_EMAIL+x} ];
+then
+  echo "Terminus variables not set up."
+else
+  terminus auth:login --email=$AMP_TERMINUS_EMAIL --machine-token=$AMP_TERMINUS_TOKEN
+fi
+
 mkdir -p ~/.lando/cache
 composer install --ignore-platform-reqs -n
 blt blt:telemetry:disable --no-interaction
 lando blt blt:telemetry:disable --no-interaction
-blt amp:landosetup $GITHUB_TOKEN $AMP_USER
+blt amp:landosetup $GITHUB_TOKEN $AMP_UID
