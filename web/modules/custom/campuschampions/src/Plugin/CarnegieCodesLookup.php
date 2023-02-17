16df829687045f1783a56dcb0178ef1962755f4c
protitude <miles.france@gmail.com>
2023-02-17 04:26:33 +0000

    campus_champions_user_export: new view - campuschampions: module override fields add Plugin to do db lookup in canegie_codes table

---
+++ web/modules/custom/campuschampions/src/Plugin/CarnegieCodesLookup.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Drupal\campuschampions\Plugin;
+
+/**
+ * Lookup in the Carnegie Codes DB.
+ *
+ * @CvsToArray (
+ *   id = "carnegie_codes_lookup",
+ *   title = @Translation("Carnegie Codes Lookup"),
+ *   description = @Translation("Lookup in the Carnegie Codes DB.")
+ * )
+ */
+
+class CarnegieCodesLookup {
+
+  /**
+   * Select db.
+   *
+   * @var \Drupal\Core\Database\Query\SelectInterface
+   */
+  private $dbLookup;
+
+  /**
+   * Constructor.
+   */
+  public function __construct() {
+    $this->dbLookup = \Drupal::database()->select('carnegie_codes', 'cc');
+  }
+
+  /**
+   * Lookup by UNITID.
+   *
+   * @return array
+   *   Fields specified.
+   */
+  public function lookupByUnitId($unitId, $fields) {
+    $this->dbLookup->fields('cc', $fields);
+    $this->dbLookup->condition('UNITID', $unitId);
+    $result = $this->dbLookup->execute()->fetchAssoc();
+    return $result;
+  }
+
+}
