50c6ba1677f348598fe575e2619c04859147f1c4
Andrew Pasquale <andrew@elytra.net>
2023-05-01 14:28:43 +0000

    Update ASP menu styles

--- web/themes/custom/accesstheme/js/accesstheme.js
+++ web/themes/custom/accesstheme/js/accesstheme.js
@@ -17,14 +17,37 @@ const checkElement = async selector => {
 // function to detect display width change
 async function handleMobileMenu() {
     if (window.innerWidth < 768) {
-        checkElement('#superfish-access-universal-nav-menu-accordion').then((selector) => {
-            selector.appendChild(li);
-        });
 
-        secondary_menu.style.display = 'block';
-        secondary_menu.classList.remove('sf-hidden');
-        secondary_menu.classList.add('sf-expanded');
-        universal_secondary_menu.style.display = 'none';
+
+
+        checkElement('#superfish-access-universal-nav-menu-accordion').then((selector) => {
+            if (!selector.querySelector('#superfish-access-additional-universal-menu-accordion-clone')) {
+                secondary_menu = document.getElementById('superfish-access-additional-universal-menu-accordion').cloneNode(true);
+                // change id of secondary menu
+                secondary_menu.id = 'superfish-access-additional-universal-menu-accordion-clone';
+                secondary_menu.style.display = 'block';
+                secondary_menu.classList.remove('sf-hidden');
+                secondary_menu.classList.add('sf-expanded');
+
+                li = document.createElement('li');
+                li.appendChild(secondary_menu);
+                selector.appendChild(li);
+
+                // add event handlers and carets to secondary menuparents
+                const i = document.createElement('i');
+                i.classList.add('fa-solid')
+                i.classList.add('fa-caret-down');
+                const menuparents = secondary_menu.querySelectorAll('li.menuparent');
+                menuparents.forEach(function (menuparent) {
+                    menuparent.addEventListener('click', function (e) {
+                        menuparent.classList.toggle('sf-expanded');
+                        menuparent.querySelector('ul').classList.toggle('sf-hidden');
+                    });
+                    menuparent.querySelector('span').appendChild(i.cloneNode(true));
+                });
+            }
+            universal_secondary_menu.style.display = 'none';
+        });
     } else {
         universal_secondary_menu.style.display = 'block';
     }
@@ -38,28 +61,6 @@ window.addEventListener('resize', function () {
 // on document ready
 document.addEventListener('DOMContentLoaded', function () {
     universal_secondary_menu = document.getElementById('block-accessadditionaluniversalmenu-2');
-    primary_menu = document.getElementById('superfish-access-universal-nav-menu-accordion');
-    secondary_menu = document.getElementById('superfish-access-additional-universal-menu-accordion').cloneNode(true);
-    // change id of secondary menu
-    secondary_menu.id = 'superfish-access-additional-universal-menu-accordion-clone';
-
-    li = document.createElement('li');
-    li.appendChild(secondary_menu);
-    primary_menu.appendChild(li);
 
     handleMobileMenu();
-
-    // add event handlers and carets to secondary menuparents
-    const i = document.createElement('i');
-    i.classList.add('fa-solid')
-    i.classList.add('fa-caret-down');
-    const menuparents = secondary_menu.querySelectorAll('li.menuparent');
-    menuparents.forEach(function (menuparent) {
-        menuparent.addEventListener('click', function (e) {
-            menuparent.classList.toggle('sf-expanded');
-            menuparent.querySelector('ul').classList.toggle('sf-hidden');
-        });
-        menuparent.querySelector('span').appendChild(i.cloneNode(true));
-    });
-
 });
