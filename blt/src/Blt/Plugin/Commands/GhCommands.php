6b33f7e9376c67cf08fab2cbb43bcd6909933883
Jasper Lieber <jasper.amp@gmail.com>
2023-05-17 21:41:14 +0000

    remove debugging

--- blt/src/Blt/Plugin/Commands/GhCommands.php
+++ blt/src/Blt/Plugin/Commands/GhCommands.php
@@ -47,9 +47,7 @@ class GhCommands extends BltTasks {
    * @description Pulls latest database artifact from Github.
    */
   public function pullfiles() {
-    $this->say("-------------- start debugging pullfiles ----------------");
     $location_url = $this->grabLocation('amp-file-backup');
-    $this->say("------ location_url = $location_url");
     $this->_exec("wget -O gh_files.zip '$location_url'");
     $this->_exec("mkdir files && unzip gh_files.zip -d files && rm gh_files.zip");
     $prev_files = 'web/sites/default/files';
@@ -57,14 +55,12 @@ class GhCommands extends BltTasks {
       $this->_exec("rm -fR $prev_files");
     }
     $this->_exec("mv files $prev_files");
-    $this->say(" -------------- end debugging pullfiles ----------------");
   }
 
   /**
    * Pull latest Database from artifacts.
    */
   private function grabLocation($name) {
-    $this->say("------ debugging grapLocation --- Grabbing github artifact location with name '$name' ");
     $artifact_call = $this->ghApiCall("https://api.github.com/repos/necyberteam/cyberteam_drupal/actions/artifacts");
     $result = json_decode($artifact_call);
     foreach ($result->artifacts as $artifact) {
@@ -76,10 +72,7 @@ class GhCommands extends BltTasks {
     $artifact_zip = $this->ghApiCall("https://api.github.com/repos/necyberteam/cyberteam_drupal/actions/artifacts/$artifact_id/zip", 1);
     $regex = '/https:\/\/.*/m';
     $location_url_search = preg_match_all($regex, $artifact_zip, $matches);
-    $this->say("----- location_url_search = " . print_r($location_url_search, TRUE));
     $location_url = $matches[0][0];
-    $this->say("------ grabLocation location_url = $location_url");
-
     return $location_url;
   }
 
@@ -87,10 +80,7 @@ class GhCommands extends BltTasks {
    * Github api call function.
    */
   private function ghApiCall($api_url, $header = 0) {
-    $this->say("------ debugging ghApiCall  ----- api_url = '$api_url' ");
     $api_token = getenv('GITHUB_TOKEN');
-    $this->say("------ debugging first 20 chars of GITHUB_TOKEN: " . substr($api_token, 0, 20));
-
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_USERPWD, $api_token);
     // Set url to post to.
@@ -105,8 +95,6 @@ class GhCommands extends BltTasks {
     ]);
     // Run the whole process.
     $result = curl_exec($ch);
-    $this->say("------ grabLocation result = " . print_r($result, TRUE));
-
     return $result;
   }
 }
