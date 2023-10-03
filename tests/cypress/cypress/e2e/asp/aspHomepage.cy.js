cd1af6cacb3700e866a221274ba1f39caa9ea206
Jasper Lieber <sourcetree@goatwalker.com>
2023-10-03 14:14:39 -0700

    cleanup

--- tests/cypress/cypress/e2e/asp/aspHomepage.cy.js
+++
@@ -1,6 +0,0 @@
-describe('Homepage AMP Test', () => {
-  it('tests amp', () => {
-    cy.visit('/')
-    cy.contains('Search the ACCESS Universe for answers by typing keywords or phrases.')
-  })
-})
