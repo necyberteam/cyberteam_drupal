53897d89055b3b0ef5204eac31d713394af78918
protitude <miles.france@gmail.com>
2023-10-10 23:46:08 +0000

    blt: add cr to ds ♽

--- blt/src/Blt/Plugin/Commands/AmpCommands.php
+++ blt/src/Blt/Plugin/Commands/AmpCommands.php
@@ -488,6 +488,7 @@ $update_list");
     }
     $default_domain = $domains[$domain_id]['id'];
     $this->_exec("$lando drush domain:default $default_domain");
+    $this->_exec("$lando drush cr");
     $this->say("Setting $default_domain as default");
     $this->_exec("$lando drush cr");
   }
