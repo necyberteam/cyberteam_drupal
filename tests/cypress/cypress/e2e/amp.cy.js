c7b5c1269d5636cb3e6ca74e4eb8440d7dc71752
Jasper Lieber <sourcetree@goatwalker.com>
2023-10-11 15:02:39 -0700

    update amp homepage text

--- tests/cypress/cypress/e2e/amp.cy.js
+++ tests/cypress/cypress/e2e/amp.cy.js
@@ -1,7 +1,7 @@
 describe('Test ASP homepage', () => {
   it('Much testing', () => {
     cy.visit('/');
-    cy.contains('Supporting the ACCESS Research Community');
+    cy.contains('Search the ACCESS Universe for answers by typing keywords or phrases. ');
   });
 });
 
