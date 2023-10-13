3e2df790ecfbd4643d46de684c82c9a01a5f23eb
Jasper Lieber <jasper.amp@gmail.com>
2023-10-13 18:40:31 +0000

    add org request cypress test

---
+++ tests/cypress/cypress/e2e/accessmatch/webforms/unauth-request-org.cy.js
@@ -0,0 +1,10 @@
+describe('The Organization Request Form', () => {
+  it('allows unauthenticated user to submit the form', () => {
+    cy.visit('/form/organization-request');
+    cy.get('#edit-your-name').type('Test');
+    cy.get('#edit-your-email').type('test@email.com');
+    cy.get('#edit-your-organization').type('Test Organization');
+    cy.get('#edit-submit').click();
+    cy.contains('Thanks for submitting your request');
+  });
+});
