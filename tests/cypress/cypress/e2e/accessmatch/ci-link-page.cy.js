4f20ebbbc1f4e1edd9f2313ad34301bd61e1cc4d
Hannah L Cameron <cameronh@student.elms.edu>
2023-10-17 15:08:52 -0400

    Fixing CI Tests

--- tests/cypress/cypress/e2e/accessmatch/ci-link-page.cy.js
+++ tests/cypress/cypress/e2e/accessmatch/ci-link-page.cy.js
@@ -1,32 +1,31 @@
 /*  
-    This test is specifically focused on the CI Links page tested for an unauthenticated user.
+    This test is specifically focused on an individual CI Links page tested for an unauthenticated user.
     This test checks for major functions like:
-    Search Bar Functionality, 
-    Radio Button Functionality,
-    Headers,
-    Sorting function, 
-    Verifying links load in,
-    And CI Links go to the correct page 
+    CI link title, 
+    CI link description,
+    Attached Affinity Group,
+    Tags, 
+    User engagement information ("0 People found this useful"), 
+    CI link category,
+    CI link Skill Level,
+    verifying images and links load in, 
+    and "Login to vote" functionality 
     
 */
-describe("Unauthenticated user tests the CI Link Page", () => {
-  it("Should test the CI Link page for unauthenticated user", () => {
+describe("Unauthenticated user tests the Individual CI Link Page", () => {
+  it("Should test Individual CI Link page for unauthenticated user", () => {
     cy.visit("/ci-links");
-    cy.contains("Votes");
-    cy.contains("Title").click();
-    cy.contains("Description");
-    cy.contains("Category").click();
-    cy.contains("Tags");
-    cy.contains("Skill Level");
-    cy.contains("Affinity Group");
-    cy.contains("Category");
-    cy.contains("Advanced").click();
-    cy.contains(
-      "There are no CI Links at this time. Please check back often as CI Links are added regularly."
-    );
-    cy.contains("Advanced").click();
-    cy.get("#edit-search--2").type("cypress-ci-link-for-testing");
     cy.contains("cypress-ci-link-for-testing").click();
     cy.contains("cypress-ci-link-for-testing");
+    cy.contains("access-acount");
+    cy.contains("finite-element-analysis");
+    cy.contains("Dummy description for ci-link 'cypress-ci-link-for-testing'");
+    cy.contains("access-acount");
+    cy.contains("0 People found this useful");
+    cy.contains("Category");
+    cy.contains("learning");
+    cy.contains("Skill Level");
+    cy.contains("Login to vote").click();
+    cy.contains("Log in");
   });
 });
