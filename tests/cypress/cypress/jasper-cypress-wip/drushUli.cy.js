05de365890a5da2ceb09f628cc9f18dd1a9d9b7a
Jasper Lieber <jasper.amp@gmail.com>
2023-10-19 16:26:45 +0000

    fixed drushUli for envs with drush

--- tests/cypress/cypress/jasper-cypress-wip/drushUli.cy.js
+++ tests/cypress/cypress/jasper-cypress-wip/drushUli.cy.js
@@ -1,16 +1,9 @@
 describe('Jasper experiments with cypress', () => {
   it('Messing about with cypress', () => {
-    // Cypress.config('baseUrl', 'https://accessmatch-local.cnctci.lndo.site');
-
     cy.task('log', 'invoking uli, baseUrl = ' + Cypress.config('baseUrl'));
     cy.drushUli();
-    // // following doesn't visit homepage
     cy.visit('/');
     cy.contains('Search the ACCESS Universe for answers by typing keywords or phrases.');
-
-    // // cy.visit(Cypress.env('baseUrl'));
     cy.contains('Log out');
-    // cy.contains('Advanced Computing Resources');
-    // cy.task('log', 'invoked uli, checked asp home page');
   });
 });
