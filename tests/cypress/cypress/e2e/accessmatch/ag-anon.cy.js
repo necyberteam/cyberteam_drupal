542580484383a530f20ddd12c96c97c0c053c4fe
Jasper Lieber <sourcetree@goatwalker.com>
2023-10-25 11:35:14 -0700

    cypress tests for affinity-groups page, anon & auth

---
+++ tests/cypress/cypress/e2e/accessmatch/ag-anon.cy.js
@@ -0,0 +1,59 @@
+//
+// Anonymous user visits the affinity-group page
+//
+describe('Anonymous user visit the affinity-group page', () => {
+  it('should find expected stuff', () => {
+
+    cy.visit('/affinity_groups');
+
+    // Verify the request a group button is present and has the correct href
+    cy.contains('Request a group')
+      .invoke('attr', 'href')
+      .should('eq', '/form/affinity-group-request');
+
+    // Verify all images appear correctly.
+    cy.get("img").each(($img) => {
+      cy.wrap($img).invoke('attr', 'src').then((src) => {
+        cy.task('log', 'verifying image "' + src + '"');
+        cy.wrap($img).scrollIntoView().should('be.visible');
+        expect($img[0].naturalWidth).to.be.greaterThan(0);
+      });
+    });
+
+    // Verify access-facilitators ag.  First get the table row
+    // containing the "access-facilitators" affinity group as an alias:
+    cy.get('[alt="The words Campus Champions ACCESS Facilitators"]')
+      .parents('tr')
+      .as('af-row');
+
+    // 1. verify the image has the correct href
+    cy.get('@af-row')
+      .find('img')
+      .parent()
+      .invoke('attr', 'href')
+      .should('contain', '/affinity-groups/access-facilitators');
+
+    // 2. verify the text has the correct href
+    cy.get('@af-row')
+      .contains('ACCESS Facilitators')
+      .invoke('attr', 'href')
+      .should('contain', '/affinity-groups/access-facilitators');
+
+    // 3. Description: verify text in description
+    cy.get('@af-row')
+      .contains('People who use or support people who use ACCESS resources and the ACCESS Resource Allocation System.');
+
+    // 4. Verify it has the expected tag
+    cy.get('@af-row')
+      .get('.square-tags')
+      .contains('research-facilitation')
+      .invoke('attr', 'href')
+      .should('contain', '/tags/research-facilitation');
+
+    // 5. verify the join button is "Login to join" for anonymous
+    cy.get('@af-row')
+      .get('[href="/user/login?destination=/affinity-groups/access-facilitators"]')
+      .invoke('attr', 'title')
+      .should('eq', 'Login to join');
+  });
+})
