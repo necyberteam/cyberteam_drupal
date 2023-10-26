e0a3b1eadb222a1caa3605b5b0d91500c146e86e
Jasper Lieber <jasper.amp@gmail.com>
2023-10-26 18:25:38 +0000

    fixing asp-homepage test to match current homepage text

--- tests/cypress/cypress/e2e/accessmatch/asp-homepage.cy.js
+++ tests/cypress/cypress/e2e/accessmatch/asp-homepage.cy.js
@@ -1,7 +1,7 @@
 describe('Homepage ASP Test', () => {
   it('tests ASP', () => {
     cy.visit('/');
-    cy.contains('Search the ACCESS Universe for answers by typing keywords or phrases.');
+    cy.contains('Not sure which ACCESS infrastructure would be best for you?');
     cy.task("log", "ASP homepage found & has expected text");
   })
 })
