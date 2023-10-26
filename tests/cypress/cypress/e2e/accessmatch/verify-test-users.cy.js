101193d63e0443d87f658769f3b1529ca3953cb7
Jasper Lieber <jasper.amp@gmail.com>
2023-10-26 23:21:50 +0000

    test tweak

--- tests/cypress/cypress/e2e/accessmatch/verify-test-users.cy.js
+++ tests/cypress/cypress/e2e/accessmatch/verify-test-users.cy.js
@@ -3,13 +3,13 @@
  */
 describe('Verify test users can login', () => {
   it('should login', () => {
-    cy.loginAs('authenticated@amptesting.com', '6%l7iF}6(4tI').then(() => {
-      cy.visit('/user/authenticatedtestuser').contains('authenticated_test_user');
-      cy.task('log', 'logged in as authenticated_test_user');
-    });
-    cy.loginAs('administrator@amptesting.com', 'b8QW]X9h7#5n').then(() => {
-      cy.visit('/user/administratortestuser').contains('administrator_test_user');
-      cy.task('log', 'logged in as administrator_test_user');
-    });
+    cy.loginAs('authenticated@amptesting.com', '6%l7iF}6(4tI');
+    cy.visit('/user/authenticatedtestuser')
+      .contains('authenticated_test_user');
+    cy.task('log', 'logged in as authenticated_test_user');
+    cy.loginAs('administrator@amptesting.com', 'b8QW]X9h7#5n');
+    cy.visit('/user/administratortestuser')
+      .contains('administrator_test_user');
+    cy.task('log', 'logged in as administrator_test_user');
   });
 });
