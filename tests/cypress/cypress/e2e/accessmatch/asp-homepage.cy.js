7fd33edbdf14f13a8a06f1533ccb69017bea62f7
Jasper Lieber <jasper.amp@gmail.com>
2023-10-27 16:54:34 +0000

    update text for asp-homepage

--- tests/cypress/cypress/e2e/accessmatch/asp-homepage.cy.js
+++ tests/cypress/cypress/e2e/accessmatch/asp-homepage.cy.js
@@ -1,7 +1,7 @@
 describe('Homepage ASP Test', () => {
   it('tests ASP', () => {
     cy.visit('/');
-    cy.contains('Search the ACCESS Universe for answers by typing keywords or phrases.');
+    cy.contains('Getting started with ACCESS');
     cy.task("log", "ASP homepage found & has expected text");
   })
 })
