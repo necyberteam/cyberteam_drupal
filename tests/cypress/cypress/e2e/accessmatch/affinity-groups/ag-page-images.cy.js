0d0ef757ac40907c61490116e939f3d75437d5fc
Jasper Lieber <jasper.amp@gmail.com>
2023-10-27 21:57:28 +0000

    fix image verification

--- tests/cypress/cypress/e2e/accessmatch/affinity-groups/ag-page-images.cy.js
+++ tests/cypress/cypress/e2e/accessmatch/affinity-groups/ag-page-images.cy.js
@@ -1,18 +1,20 @@
 /*
 * Anonymous user visits the affinity-group page and verifies images.
 */
-describe('Anonymous user visit the affinity-group page', () => {
+describe('Anonymous user verifes the images on the affinity-group page', () => {
   it('should find expected stuff', () => {
     cy.visit('/affinity_groups');
 
     cy.get('img').each(($img) => {
-      const url = $img[0]?.src || $img[0]?.srcset;
+      const url = ($img[0]?.src || $img[0]?.srcset);
       if (url) {
-        cy.task('log', 'verifying image "' + url + '"').then(() => {
-          cy.wrap($img).scrollIntoView().should('be.visible').then(() => {
-            expect($img[0].naturalWidth).to.be.greaterThan(0);
-            expect($img[0].naturalHeight).to.be.greaterThan(0);
-          });
+        cy.request(url).then((response) => {
+          // verify the image response is 200, and has a non-zero length body
+          // cy.task('log', 'verifying image "' + url + '"').then(() => {
+          expect(response.status).to.eq(200);
+          expect(response.body.length).to.be.greaterThan(0);
+
+          // });
         });
       } else {
         throw new Error(`Found an image with no url src`);
