ac165a313d2e476be6bc9b22cdd957e5d2b0d849
Hannah L Cameron <cameronh@student.elms.edu>
2023-11-01 15:59:20 -0400

    Removing disabled lines from test

--- tests/cypress/cypress/e2e/accessmatch/webforms/admin-create-ci-link.cy.js
+++ tests/cypress/cypress/e2e/accessmatch/webforms/admin-create-ci-link.cy.js
@@ -34,10 +34,18 @@ describe("Admin user uses form to create a CI Link", () => {
     cy.get("#edit-link-to-resource-add-submit").click();
 
     // // add the 2 links
-    // cy.get('input[id^="edit-link-to-resource-items-1-item-title"]').type('title for ci-link link 1');
-    // cy.get('input[name="link_to_resource[items][1][_item_][url]"]').type('http://example-1.com');
-    // cy.get('input[name="link_to_resource[items][2][_item_][title]"').type('title for ci-link link 2');
-    // cy.get('input[name="link_to_resource[items][2][_item_][url]"]').type('http://example-2.com');
+    cy.get('input[id^="edit-link-to-resource-items-1-item-title"]').type(
+      "title for ci-link link 1"
+    );
+    cy.get('input[name="link_to_resource[items][1][_item_][url]"]').type(
+      "http://example-1.com"
+    );
+    cy.get('input[name="link_to_resource[items][2][_item_][title]"').type(
+      "title for ci-link link 2"
+    );
+    cy.get('input[name="link_to_resource[items][2][_item_][url]"]').type(
+      "http://example-2.com"
+    );
 
     // tag "access-acount" is selected
     cy.get('span[data-tid="733"]').click();
@@ -51,34 +59,34 @@ describe("Admin user uses form to create a CI Link", () => {
     // cy.visit('/webform/ci-link/submissions/1/edit');
 
     // verify submission looks good
-    // cy.contains("cypress-ci-link-for-testing");
-    // cy.contains("Dummy description for ci-link 'cypress-ci-link-for-testing'");
-    // cy.contains("title for ci-link link 0").should(
-    //   "have.attr",
-    //   "href",
-    //   "http://example-0.com"
-    // );
-    // cy.contains("title for ci-link link 1").should(
-    //   "have.attr",
-    //   "href",
-    //   "http://example-1.com"
-    // );
-    // cy.contains("title for ci-link link 2").should(
-    //   "have.attr",
-    //   "href",
-    //   "http://example-2.com"
-    // );
-    // cy.contains("access-acount")
-    //   .should("have.attr", "href")
-    //   .and("contains", "/tags/access-acount");
-    // cy.contains("finite-element-analysis")
-    //   .should("have.attr", "href")
-    //   .and("contains", "/tags/finite-element-analysis");
-    // cy.contains("Beginner")
-    //   .should("have.attr", "href")
-    //   .and("contains", "/skill-level/beginner");
-    // cy.contains("Intermediate")
-    //   .should("have.attr", "href")
-    //   .and("contains", "/skill-level/intermediate");
+    cy.contains("cypress-ci-link-for-testing");
+    cy.contains("Dummy description for ci-link 'cypress-ci-link-for-testing'");
+    cy.contains("title for ci-link link 0").should(
+      "have.attr",
+      "href",
+      "http://example-0.com"
+    );
+    cy.contains("title for ci-link link 1").should(
+      "have.attr",
+      "href",
+      "http://example-1.com"
+    );
+    cy.contains("title for ci-link link 2").should(
+      "have.attr",
+      "href",
+      "http://example-2.com"
+    );
+    cy.contains("access-acount")
+      .should("have.attr", "href")
+      .and("contains", "/tags/access-acount");
+    cy.contains("finite-element-analysis")
+      .should("have.attr", "href")
+      .and("contains", "/tags/finite-element-analysis");
+    cy.contains("Beginner")
+      .should("have.attr", "href")
+      .and("contains", "/skill-level/beginner");
+    cy.contains("Intermediate")
+      .should("have.attr", "href")
+      .and("contains", "/skill-level/intermediate");
   });
 });
