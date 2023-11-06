32159c24971a1a4a3259a06899855b1534b26a24
Hannah L Cameron <cameronh@student.elms.edu>
2023-11-06 14:02:39 -0500

    Fixing typo in help ticket

--- tests/cypress/cypress/e2e/accessmatch/help-ticket.cy.js
+++ tests/cypress/cypress/e2e/accessmatch/help-ticket.cy.js
@@ -5,25 +5,29 @@
 */
 describe("Verify the buttons on the Help Ticket page update the form action to go to expected pages", () => {
   it("Tests the Help Ticket page", () => {
-
     cy.visit("/help-ticket");
 
-    cy.contains("I need help logging into ACCESS website")
-      .click();
-    cy.get('#ticket-choice-form')
-      .invoke('attr', 'action')
-      .should('equal', 'https://access-ci.atlassian.net/servicedesk/customer/portal/2/group/3/create/30');
+    cy.contains("I need help logging into ACCESS website").click();
+    cy.get("#ticket-choice-form")
+      .invoke("attr", "action")
+      .should(
+        "equal",
+        "https://access-ci.atlassian.net/servicedesk/customer/portal/2/group/3/create/30"
+      );
 
-    cy.contains("I need help logging in to other affiliated infrastructure")
-      .click();
-    cy.get('#ticket-choice-form')
-      .invoke('attr', 'action')
-      .should('equal', 'https://access-ci.atlassian.net/servicedesk/customer/portal/2/group/3/create/31');
+    cy.contains(
+      "I need help logging into other affiliated infrastructure"
+    ).click();
+    cy.get("#ticket-choice-form")
+      .invoke("attr", "action")
+      .should(
+        "equal",
+        "https://access-ci.atlassian.net/servicedesk/customer/portal/2/group/3/create/31"
+      );
 
-    cy.contains("I have another question")
-      .click();
-    cy.get('#ticket-choice-form')
-      .invoke('attr', 'action')
-      .should('equal', '/open-a-ticket')
+    cy.contains("I have another question").click();
+    cy.get("#ticket-choice-form")
+      .invoke("attr", "action")
+      .should("equal", "/open-a-ticket");
   });
 });
