b55f8751db28c5f7242ebfae7e56f6f090a713c0
Hannah L Cameron <cameronh@student.elms.edu>
2023-11-08 15:13:51 -0500

    CI Link Indie Auth fix

--- tests/cypress/cypress/e2e/accessmatch/knowledge-base/ci-links/ci-link-indie-auth.cy.js
+++ tests/cypress/cypress/e2e/accessmatch/knowledge-base/ci-links/ci-link-indie-auth.cy.js
@@ -5,39 +5,39 @@ describe("Authenticated user tests a ci link", () => {
   it("verify auth features of a ci link", () => {
     // first create a dummy ci-links so can reference one of them in the AG.
     create_dummy_ci_link();
-
     // login user with the "authenticated" role
     cy.loginAs("authenticated@amptesting.com", "6%l7iF}6(4tI");
-
     //Navigating to cypress created ci link
     cy.visit("/ci-links");
-    cy.get(":nth-child(2) > .views-field-webform-submission-value-5 > a")
+    cy.get('[headers="view-webform-submission-value-5-table-column"] > a')
       .contains("dummy-ci-link-for-testing-knowledge-base")
-      .click();
-
-    //Vote feature
-    cy.get(".flag > .flex").click();
-    cy.contains("Thanks! Your vote has been recorded.");
-
-    //Flag Feature.When one of the selections in the dropdown is clicked I am brough to an access denied page. Section is disabled for now
-    cy.get(".ps-5").contains("Flag this CI Link").click();
-    cy.get(".dropdown-menu").contains("Flag this item not useful");
-
-    // helper function to create a ci-link
-    function create_dummy_ci_link() {
-      cy.loginAs("administrator@amptesting.com", "b8QW]X9h7#5n");
-      cy.visit("/form/ci-link");
-      cy.get("#edit-approved").check();
-      cy.get("#edit-title").type("dummy-ci-link-for-testing-knowledge-base");
-      cy.get("#edit-category").select("Learning");
-      cy.get("#edit-skill-level-304").check(); // beginner level
-      cy.get("#edit-description").type(
-        "Dummy description for ci-link 'dummy-ci-link-for-testing-knowledge-base'"
-      );
-      // tag "access-acount" is selected
-      cy.get(".tags").contains("access-acount").click();
-      cy.get("#edit-submit").click();
-      cy.drupalLogout();
-    }
+      .click()
+      .then(() => {
+        // cy.get(":nth-child(2) > .views-field-webform-submission-value-5 > a")
+        //   .contains("dummy-ci-link-for-testing-knowledge-base")
+        //   .click();
+        //Vote feature
+        cy.get(".flag > .flex").click();
+        cy.contains("Thanks! Your vote has been recorded.");
+        //Flag Feature.When one of the selections in the dropdown is clicked I am brough to an access denied page. Section is disabled for now
+        cy.get(".ps-5").contains("Flag this CI Link").click();
+        cy.get(".dropdown-menu").contains("Flag this item not useful");
+      });
   });
 });
+// helper function to create a ci-link
+function create_dummy_ci_link() {
+  cy.loginAs("administrator@amptesting.com", "b8QW]X9h7#5n");
+  cy.visit("/form/ci-link");
+  cy.get("#edit-approved").check();
+  cy.get("#edit-title").type("dummy-ci-link-for-testing-knowledge-base");
+  cy.get("#edit-category").select("Learning");
+  cy.get("#edit-skill-level-304").check(); // beginner level
+  cy.get("#edit-description").type(
+    "Dummy description for ci-link 'dummy-ci-link-for-testing-knowledge-base'"
+  );
+  // tag "access-acount" is selected
+  cy.get(".tags").contains("access-acount").click();
+  cy.get("#edit-submit").click();
+  cy.drupalLogout();
+}
