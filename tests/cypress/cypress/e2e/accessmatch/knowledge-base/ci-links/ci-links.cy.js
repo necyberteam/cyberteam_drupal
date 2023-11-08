6f12d78e90f3bf474bf51972a64407d2693bc2b9
Hannah L Cameron <cameronh@student.elms.edu>
2023-11-08 13:50:08 -0500

    CI links fix

--- tests/cypress/cypress/e2e/accessmatch/knowledge-base/ci-links/ci-links.cy.js
+++ tests/cypress/cypress/e2e/accessmatch/knowledge-base/ci-links/ci-links.cy.js
@@ -10,6 +10,7 @@
 */
 describe("Unauthenticated user tests the CI Links Page", () => {
   it("Should test the CI Links page for unauthenticated user", () => {
+    create_dummy_ci_link();
     cy.visit("/ci-links");
     //Testing Column Headers
     cy.contains("Votes");
@@ -27,14 +28,28 @@ describe("Unauthenticated user tests the CI Links Page", () => {
     );
     cy.get(".js-form-item-skill-level-306").contains("Advanced").click();
     //Searching for Cypress Created CI Link
-    cy.get("#edit-search--2").type("dummy-ci-link-for-testing-knowledge-base");
+    cy.get("#edit-search--2").type("access-support-ci-link-for-testing");
     cy.get("tbody > :nth-child(1) > .views-field-webform-submission-value-5")
-      .contains("dummy-ci-link-for-testing-knowledge-base")
+      .contains("access-support-ci-link-for-testing")
       .click()
       .then(() => {
-        cy.get(".page-title").contains(
-          "dummy-ci-link-for-testing-knowledge-base"
-        );
+        cy.get(".page-title").contains("access-support-ci-link-for-testing");
       });
   });
 });
+// helper function to create a ci-link
+function create_dummy_ci_link() {
+  cy.loginAs("administrator@amptesting.com", "b8QW]X9h7#5n");
+  cy.visit("/form/ci-link");
+  cy.get("#edit-approved").check();
+  cy.get("#edit-title").type("access-support-ci-link-for-testing");
+  cy.get("#edit-category").select("Learning");
+  cy.get("#edit-skill-level-304").check(); // beginner level
+  cy.get("#edit-description").type(
+    "Dummy description for ci-link 'access-support-ci-link-for-testing'"
+  );
+  // tag "access-acount" is selected
+  cy.get('span[data-tid="733"]').click();
+  cy.get("#edit-submit").click();
+  cy.drupalLogout();
+}
