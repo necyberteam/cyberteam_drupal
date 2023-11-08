dfe692a3aff4dafb0d2f1f1a23a7bda05e16ce64
Jasper Lieber <sourcetree@goatwalker.com>
2023-11-08 13:00:07 -0800

    tags test

--- tests/cypress/cypress/e2e/accessmatch/tags/tags.cy.js
+++ tests/cypress/cypress/e2e/accessmatch/tags/tags.cy.js
@@ -5,4 +5,48 @@ describe("Verify the tags page", () => {
   it("Tests the tags page", () => {
 
     cy.visit("/tags");
+
+    cy.get(".page-title").contains("Tags");
+
+    // check breadcrumbs
+    const crumbs = [
+      ['Support', '/'],
+      ['Tags', null]
+    ];
+    cy.checkBreadcrumbs(crumbs);
+
+    // each h2 is a tag category -- verify each h2 with the each() function
+    cy.get('.block-system-main-block')
+      .find('h2')
+      .each(($el) => {
+
+        // verify there's a block with the id of the h2 text
+        cy.get('.block-system-main-block')
+          .get('[id="' + $el.text() + '"]')
+          .contains($el.text());
+
+        // each category should have a bunch of tags - get an alias to that element
+        cy.get('.block-system-main-block')
+          .get('[id="' + $el.text() + '"]')
+          .parents(".views-field.views-field-name")
+          .next() // in the DOM, this gets the element with the tags
+          .as('tag-list');
+
+        // verify there are more than 0 tags per category
+        cy.get('@tag-list')
+          .find('a').should('have.length.gt', 0);
+
+        // verify each tag has a good href
+        cy.get('@tag-list')
+          .find('a')
+          .each($a => {
+            expect($a).to.have.attr("href", '/tags/' + $a.text());
+          });
+
+        // verify 2nd sidebar has all same tag category
+        cy.get('.region.region-sidebar-second')
+          .contains($el.text())
+          .should('have.attr', 'href', '#' + $el.text());
+      });
   });
+});
