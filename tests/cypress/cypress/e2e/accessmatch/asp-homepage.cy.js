545dd2d9414510025e20a23b4d7baa1d3003a938
Hannah L Cameron <cameronh@student.elms.edu>
2023-11-13 13:17:04 -0500

    Homepage is tested

---
+++ tests/cypress/cypress/e2e/accessmatch/asp-homepage.cy.js
@@ -0,0 +1,41 @@
+/*  
+    This test is specifically focused on the ASP Homepage tested for an unauthenticated user.
+    This test checks for major functions like: 
+    
+    Announcements and Events block,
+    Call to actions,
+    Search Access
+        
+*/
+describe("Unauthenticated user tests the ASP Homepage", () => {
+  it("Should test the ASP Homepage for unauthenticated user", () => {
+    cy.visit("/");
+
+    //Announcements block section
+    cy.get(".block-views-blockaccess-news-latest-news-block").contains(
+      "announcements"
+    );
+    cy.get(".block-views-blockaccess-news-latest-news-block").contains(
+      "See all announcements"
+    );
+
+    //Events block section
+    cy.get(
+      ".block-views-blockrecurring-events-event-instances-latest-events-block"
+    ).contains("Upcoming Events");
+    cy.get(
+      ".block-views-blockrecurring-events-event-instances-latest-events-block"
+    ).contains("See all events");
+
+    //Search Access block section and button destination
+    cy.get(":nth-child(1) > .mt-0").contains("Search ACCESS");
+
+    cy.get(".clearfix > .grid > :nth-child(1) > .btn")
+      .contains("Search")
+      .should("have.attr", "href", "/find");
+
+    //Call to action button  (function should be created to test more call to actions)
+    cy.get("#cta").contains("Try the ACCESS Resource Advisor");
+    //.should("have.attr", "href", "access-ara.ccs.uky.edu:8080/");
+  });
+});
