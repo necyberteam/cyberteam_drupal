df174a0ad7900c67b4faa481c57f34b689a3691f
Jasper Lieber <sourcetree@goatwalker.com>
2023-11-14 12:19:48 -0800

    test of find

---
+++ tests/cypress/cypress/e2e/accessmatch/find.cy.js
@@ -0,0 +1,29 @@
+/*
+    Test /find
+ */
+describe("Test of the /find page", () => {
+  it("Should complete successfully", () => {
+
+    cy.visit("/find");
+
+    cy.get('#block-pagetitle').contains("Find Information about ACCESS");
+
+    // check breadcrumbs
+    const crumbs = [
+      ['Support', '/'],
+      ['Find Information about ACCESS', null]
+    ];
+    cy.checkBreadcrumbs(crumbs);
+
+    cy.get('.button').contains('Search');
+
+    cy.get('.sui-search-box__text-input')
+      .type('test{enter}');
+
+    cy.get('.btn').contains('Hide Results');
+    cy.get('.sui-paging-info').contains('Showing');
+    cy.get('.sui-select').contains('Relevance');
+    cy.get('.sui-layout-sidebar').contains('ACCESS Home');
+  });
+});
+
