057aa2cc32d63516fab9b5db81515fcea1773ac2
Hannah L Cameron <cameronh@student.elms.edu>
2023-11-16 13:48:04 -0500

    Adding testing for community, match,video

---
+++ tests/cypress/cypress/e2e/accessmatch/community-overview.cy.js
@@ -0,0 +1,25 @@
+/*
+    This test is specifically focused on the Community Overview Page tested for an unauthenticated user.
+    This test checks for major functions like:
+    Page Title,
+    CTA Functionality
+    Popular Tags
+    */
+
+describe("Unauthenticated user tests the Community Overview Page", () => {
+  it("Should test Community Overview Page for unauthenticated user", () => {
+    // login user with the "unauthenticated" role
+    cy.visit("/community/overview");
+
+    //Page Title
+
+    //Call to action function
+    cy.verifyCallToActionBlock(
+      "/community/overview",
+      "Join the CSSN",
+      "https://localhost/user/login?destination=/cssn%23join-cssn"
+    );
+
+    //Popular Tags tested?
+  });
+});
