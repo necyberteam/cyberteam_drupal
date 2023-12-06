fc55202ea833d488c42de55c5d2d5973188b5342
Andrew Pasquale <andrew@elytra.net>
2023-12-06 15:40:31 -0500

    Fix events tests

--- tests/cypress/cypress/e2e/accessmatch/events/events-page.cy.js
+++ tests/cypress/cypress/e2e/accessmatch/events/events-page.cy.js
@@ -8,59 +8,55 @@
     Test breadcrumbs
 
 */
-describe("Unauthenticated user tests the Events Page", () => {
-  it("Should test the EVents page for unauthenticated user", () => {
-    cy.visit("/events");
+describe('Unauthenticated user tests the Events Page', () => {
+  it('Should test the EVents page for unauthenticated user', () => {
+    cy.visit('/events');
 
     //Page title and column headers
-    cy.get(".page-title").contains("Events & Trainings");
-    cy.get("#view-title-2-table-column").contains("Title");
-    cy.get("#view-date-value-1-table-column").contains("Event Date");
-    cy.get("#view-body-format-table-column").contains("Description");
+    cy.get('.page-title').contains('Events & Trainings');
+    cy.get('#view-title-2-table-column').contains('Title');
+    cy.get('#view-date-value-1-table-column').contains('Event Date');
+    cy.get('#view-body-format-table-column').contains('Description');
 
     //Filter feature
-    cy.get("#edit-field-affiliation-value--2").select("Community"); //Affiliation field
-    cy.get("#edit-field-event-type-value--2").select("Training"); //Event Type
-    cy.get("#edit-tid--2").select("login"); //Tag field
-    cy.get("#edit-field-skill-level-value--2").select("Advanced"); //Skill Level
-    cy.get("#edit-submit-recurring-events-event-instances--2").click(); //Submit button
+    cy.get('#edit-field-affiliation-value--2').select('Community'); //Affiliation field
+    cy.get('#edit-field-event-type-value--2').select('Training'); //Event Type
+    cy.get('#edit-tid--2').select('login'); //Tag field
+    cy.get('#edit-field-skill-level-value--2').select('Advanced'); //Skill Level
+    cy.get('#edit-submit-recurring-events-event-instances--2').click(); //Submit button
 
     //Testing Cypress Created Event
     //Event Title
-    cy.get("tbody > tr > .views-field-title-2")
-      .contains("example-event")
+    cy.get('tbody > tr > .views-field-title-2')
+      .contains('example-event')
       .click();
-    cy.get(".page-title > .field").contains("example-event");
+    cy.get('.page-title > .field').contains('example-event');
 
     //Event Date
-    cy.get(".field--name-date").contains("Event Date");
-    cy.get(".field--name-date").contains(
-      "12/12/26"
+    cy.get('.field--name-date').contains(
+      '12/12/26'
     );
 
     //Event Location
-    cy.get(".field--name-location").contains("Location");
-    cy.get(".field--name-location").contains("Zoom");
+    cy.get('.field--name-location').contains('Zoom');
 
     //Event Tags
-    cy.get(".field--name-tags").contains("Tags");
-    cy.get(".field--name-tags").contains("login");
+    cy.get('.field__item.me-2 a').contains('login');
 
     //Event Contact
-    cy.get(".field--name-contact").contains("Contact");
-    cy.get(".field--name-contact").contains("Pecan Pie");
+    cy.get('.field--name-contact').contains('Contact');
+    cy.get('.field--name-contact').contains('Pecan Pie');
 
     //Registration Button Not sure how to correctly reference the btn
-    cy.get(".field--name-registration").contains("Registration");
-    cy.get(".field--name-registration").contains("Register here")
-      .should("have.attr", "href");
+    cy.get('.field__item .btn').contains('Register')
+      .should('have.attr', 'href');
 
     //Event Skill Level
-    cy.get(".field--name-skill-level").contains("Skill Level");
-    cy.get(".field--name-skill-level").contains("Advanced");
+    cy.get('.field--name-skill-level').contains('Skill Level');
+    cy.get('.field--name-skill-level img[alt="Advanced"]').should('be.visible');
 
     //Event Type
-    cy.get(".field--name-event-type").contains("Event Type");
-    cy.get(".field--name-event-type").contains("Training");
+    cy.get('.field--name-event-type').contains('Event Type');
+    cy.get('.field--name-event-type').contains('Training');
   });
 });
