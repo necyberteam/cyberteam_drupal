3bc1252b376ba2793e2470c89dc8f7bc8a37ed7c
Andrew Pasquale <andrew@elytra.net>
2024-01-05 10:57:55 -0500

    Fix spelling

--- tests/cypress/cypress/e2e/accessmatch/announcements/announcement-form.cy.js
+++ tests/cypress/cypress/e2e/accessmatch/announcements/announcement-form.cy.js
@@ -23,7 +23,7 @@ describe("Authenticated user tests the Announcement Form without adding an Affin
     cy.get(".page-title").contains("Create ACCESS Announcements");
 
     //Form Title Field
-    cy.get("#edit-title-0-value").type("Cypress-Created-Annoucement");
+    cy.get("#edit-title-0-value").type("Cypress-Created-Announcement");
 
     //Form Image Field
     cy.get("#ajax-wrapper > .claro-details").contains("Featured Image");
