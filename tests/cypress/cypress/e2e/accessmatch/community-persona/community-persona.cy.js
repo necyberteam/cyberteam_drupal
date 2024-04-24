cf03cd36c7bfbce0d518f30e89684be88265ade9
Andrew Pasquale <andrew@elytra.net>
2024-04-24 14:01:42 -0400

    Fix breadcumb test for community persona

--- tests/cypress/cypress/e2e/accessmatch/community-persona/community-persona.cy.js
+++ tests/cypress/cypress/e2e/accessmatch/community-persona/community-persona.cy.js
@@ -65,21 +65,10 @@ function anon_verify_user201_cp() {
 function user200_verify_self_cp() {
   cy.loginAs('walnut@pie.org', 'Walnut');
 
-  // verify breadcrumbs for self
-
-  cy.visit('/community-persona/200');
-  // verify breadcrumbs for self
-  let crumbs = [
-    ['Support', '/'],
-    ['Community Persona', '/community-persona'],
-    ['Walnut Pie', null]
-  ];
-  cy.checkBreadcrumbs(crumbs);
-
   cy.visit('/community-persona');
 
   // verify breadcrumbs for self
-  crumbs = [
+  let crumbs = [
     ['Support', '/'],
     ['Community Persona', null]
   ];
