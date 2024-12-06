96c19380fa581619fa315209a7c381197d7bf54c
protitude <miles.france@gmail.com>
2024-12-06 10:34:36 -0700

    cypress: adding more to people

--- tests/cypress/cypress/e2e/templates/people/people-card-auth.cy.js
+++ tests/cypress/cypress/e2e/templates/people/people-card-auth.cy.js
@@ -18,9 +18,26 @@ describe("test people page Card view w/ filters", () => {
     cy.contains('Julie Ma')
 
     cy.get('#edit-search-api-fulltext--3').clear()
+    cy.wait(5000);
     cy.get('#program-308').click();
-    cy.wait(700);
+    cy.wait(5000);
     cy.contains('Programs Northeast')
+    cy.get('#program-reset-all').check();
+    cy.wait(5000);
+
+    cy.get('.js-facet-block-id-nect_organizationcyberteampeople > .mb-4 > .facets-soft-limit-link').click();
+    cy.get('#organization-cyberteam-people-1931').check();
+    cy.wait(2000);
+    cy.contains('Harvard University')
+    cy.get('#organization-cyberteam-people-reset-all').check();
+    cy.wait(2000);
+
+    cy.get('.js-facet-block-id-nect_userskillscyberteampeople > .mb-4 > .facets-soft-limit-link').click();
+    cy.get('#user-skills-cyberteam-people-llm').check();
+    cy.wait(2000);
+    cy.get('.views-element-container').contains('llm');
+    cy.get('#user-skills-cyberteam-people-bash').check();
+
   });
 
 
