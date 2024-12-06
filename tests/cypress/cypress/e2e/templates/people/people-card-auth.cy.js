0289a0aa359309801609148bc1e10dece80381ee
protitude <miles.france@gmail.com>
2024-12-06 11:07:06 -0700

    cypress: people-card-auth d8-2256

--- tests/cypress/cypress/e2e/templates/people/people-card-auth.cy.js
+++ tests/cypress/cypress/e2e/templates/people/people-card-auth.cy.js
@@ -9,34 +9,31 @@ describe("test people page Card view w/ filters", () => {
     cy.contains('Organization')
 
     cy.get('#edit-search-api-fulltext--3').type('testing123')
-    cy.wait(700);
+    cy.wait(1000)
     cy.contains('No matches found')
 
     cy.get('#edit-search-api-fulltext--3').clear()
+    cy.wait(1000)
     cy.get('#edit-search-api-fulltext--3').type('julie')
-    cy.wait(700);
+    cy.wait(1000)
     cy.contains('Julie Ma')
 
     cy.get('#edit-search-api-fulltext--3').clear()
-    cy.wait(5000);
+    cy.wait(1000)
+
     cy.get('#program-308').click();
-    cy.wait(5000);
     cy.contains('Programs Northeast')
-    cy.get('#program-reset-all').check();
-    cy.wait(5000);
+    cy.get('#program-reset-all').click();
 
-    cy.get('.js-facet-block-id-nect_organizationcyberteampeople > .mb-4 > .facets-soft-limit-link').click();
-    cy.get('#organization-cyberteam-people-1931').check();
-    cy.wait(2000);
+    cy.get('#block-nect-organizationcyberteampeople > .mb-4 > .facets-soft-limit-link').click();
+    cy.get('#organization-cyberteam-people-1931').click();
     cy.contains('Harvard University')
-    cy.get('#organization-cyberteam-people-reset-all').check();
-    cy.wait(2000);
+    cy.get('#organization-cyberteam-people-reset-all').click();
 
-    cy.get('.js-facet-block-id-nect_userskillscyberteampeople > .mb-4 > .facets-soft-limit-link').click();
-    cy.get('#user-skills-cyberteam-people-llm').check();
-    cy.wait(2000);
-    cy.get('.views-element-container').contains('llm');
-    cy.get('#user-skills-cyberteam-people-bash').check();
+    cy.get('#block-nect-userskillscyberteampeople > .mb-4 > .facets-soft-limit-link').click();
+    cy.get('#user-skills-cyberteam-people-llm').click();
+    cy.contains('llm')
+    cy.get('#user-skills-cyberteam-people-bash').click();
 
   });
 
