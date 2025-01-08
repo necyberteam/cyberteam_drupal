207058a59d2a9f9715ecd0203fdd943a1918c4ee
Andrew Pasquale <andrew@elytra.net>
2025-01-08 10:28:09 -0500

    Logging for Join the CSSN test

--- tests/cypress/cypress/e2e/accessmatch1/community/cssn/join-cssn-form.cy.js
+++ tests/cypress/cypress/e2e/accessmatch1/community/cssn/join-cssn-form.cy.js
@@ -36,6 +36,12 @@ describe("Authenticated user tests the form to join the CSSN", () => {
 
     // Check the community persona to see if the program/region was added.
     cy.visit("/community-persona");
+    const institution = cy.get(".persona .institution")
+    cy.log(institution.text())
+    const d = cy.get(".persona .institution + div")
+    cy.log(d.text())
+    const p = cy.get(".persona .institution + div > p")
+    cy.log(p.text())
     // Check that CSSN member is displayed on community persona.
     cy.get(".persona .institution + div > p > strong").contains("CSSN Member")
     // Check that the affinity group was flagged.
