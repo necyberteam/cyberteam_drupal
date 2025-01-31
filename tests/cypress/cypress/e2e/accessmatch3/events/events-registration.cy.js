0c89f9b77ed929380f407b372f195cdef7367996
protitude <miles.france@gmail.com>
2025-01-31 13:55:10 -0700

    cypress: d8-2329
    Registration check only as admin for now

--- tests/cypress/cypress/e2e/accessmatch3/events/events-registration.cy.js
+++ tests/cypress/cypress/e2e/accessmatch3/events/events-registration.cy.js
@@ -1,6 +1,8 @@
 describe('Test the registration feature', () => {
-  it('Register for an event as Pecan', () => {
-    cy.loginAs('pecan@pie.org', 'Pecan');
+
+  it('Approve registration as admin', () => {
+    cy.loginAs("administrator@amptesting.com", "b8QW]X9h7#5n");
+
     cy.visit('/events')
     cy.get('#edit-search-api-fulltext--2').type('example')
     cy.wait(1000)
@@ -11,20 +13,10 @@ describe('Test the registration feature', () => {
 
     cy.contains('You will receive an email after your registration is approved')
     cy.contains('Approved: No')
-  })
 
-  it('Approve registration as admin', () => {
-    cy.loginAs("administrator@amptesting.com", "b8QW]X9h7#5n");
-    cy.visit('/events')
-    cy.get('#edit-search-api-fulltext--2').type('example')
-    cy.wait(1000)
-    cy.contains('cypress-example-event').click()
     cy.contains('Registrations').click()
     cy.contains('Approve All').click()
-  })
 
-  it('Check approval as pecan', () => {
-    cy.loginAs('pecan@pie.org', 'Pecan');
     cy.visit('/events')
     cy.get('#edit-search-api-fulltext--2').type('example')
     cy.wait(1000)
@@ -32,4 +24,5 @@ describe('Test the registration feature', () => {
 
     cy.contains('Approved: Yes')
   })
+
 })
