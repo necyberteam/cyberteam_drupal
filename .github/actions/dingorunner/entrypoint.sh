21bb425077506aaef2c96c94835f7a5f61a8bcc6
protitude <miles.france@gmail.com>
2025-02-28 15:40:45 -0700

    github: add version - no build - sync to updates.yml

--- .github/actions/dingorunner/entrypoint.sh
+++ .github/actions/dingorunner/entrypoint.sh
@@ -87,7 +87,7 @@ then
   git config --global user.name \"$username\"
   composer config -g github-oauth.github.com $gh_token
   composer install --no-dev --ignore-platform-reqs
-  $blt amp:ciupdate "$drupal_update" --no-interaction --verbose
+  $blt amp:ciupdate "$drupal_update" $version --no-interaction --verbose
   git push origin $drupal_update
 fi
 
