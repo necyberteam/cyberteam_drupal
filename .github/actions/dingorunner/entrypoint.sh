f3376bdb075b3c27a868315b92df9ff465f8855c
protitude <miles.france@gmail.com>
2025-03-07 15:59:51 -0700

    github: fix compuser_update action #nobuild

--- .github/actions/dingorunner/entrypoint.sh
+++ .github/actions/dingorunner/entrypoint.sh
@@ -163,7 +163,7 @@ then
   git config --global user.name \"$username\"
   composer config -g github-oauth.github.com $gh_token
   composer install --no-dev --ignore-platform-reqs
-  $robo ciupdate "$drupal_update" --no-interaction --verbose
+  vendor/bin/robo ciupdate "$drupal_update" --no-interaction --verbose
   git push origin $drupal_update
 fi
 
