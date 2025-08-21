2d7fea5d419164835410256adb366a0c07453ee9
protitude <miles.france@gmail.com>
2025-08-21 18:01:17 -0400

    cypress: d8-2332
    accessmatch: register for event

--- tests/cypress/cypress/e2e/accessmatch3/events/events-registration.cy.js
+++ tests/cypress/cypress/e2e/accessmatch3/events/events-registration.cy.js
@@ -23,4 +23,42 @@ describe('Test the registration feature', () => {
     cy.contains('Approved: Yes')
   })
 
+  it('Walnut register for event', () => {
+    cy.loginAs("walnut@pie.org", "Walnut");
+
+    cy.visit('/events')
+    cy.get('#edit-search-api-fulltext--2').type('example', { delay: 0 })
+    cy.wait(1000)
+    cy.contains('cypress-example-event').click()
+    cy.contains('Register').click()
+    cy.contains('Please confirm your registration below')
+    cy.get('#edit-submit').click()
+
+    cy.contains('You will receive an email after your registration is approved')
+    cy.contains('Approved: No')
+  })
+
+  it('Approve Walnut registration as admin', () => {
+    cy.loginAs("administrator@amptesting.com", "b8QW]X9h7#5n");
+
+    cy.visit('/events')
+    cy.get('#edit-search-api-fulltext--2').type('example', { delay: 0 })
+    cy.wait(1000)
+    cy.contains('cypress-example-event').click()
+
+    cy.contains('Registrations').click()
+    cy.contains('Approve All').click()
+  })
+
+  it('Walnut check registration status', () => {
+    cy.loginAs("walnut@pie.org", "Walnut");
+
+    cy.visit('/events')
+    cy.get('#edit-search-api-fulltext--2').type('example', { delay: 0 })
+    cy.wait(1000)
+    cy.contains('cypress-example-event').click()
+
+    cy.contains('Approved: Yes')
+  })
+
 })
