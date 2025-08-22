534726eaf7fab1f1677052c96a199485318fcddd
protitude <miles.france@gmail.com>
2025-08-22 10:03:08 -0400

    cypress: d8-2332
    registration: add to waitlist and check for emails

--- tests/cypress/cypress/e2e/accessmatch3/events/events-registration.cy.js
+++ tests/cypress/cypress/e2e/accessmatch3/events/events-registration.cy.js
@@ -1,6 +1,8 @@
 describe('Test the registration feature', () => {
 
   it('Approve registration as admin', () => {
+    cy.clearMailpit();
+
     cy.loginAs("administrator@amptesting.com", "b8QW]X9h7#5n");
 
     cy.visit('/events')
@@ -14,6 +16,19 @@ describe('Test the registration feature', () => {
     cy.contains('You will receive an email after your registration is approved')
     cy.contains('Approved: No')
 
+    // Check for pending registration email
+    cy.waitForEmail({
+      to: 'administrator@amptesting.com',
+      subject: 'Registration Pending for cypress-example-event'
+    }).then((message) => {
+      cy.assertEmailContent(message, {
+        subject: 'Registration Pending for cypress-example-event',
+        from: 'noreply@support.access-ci.org',
+        to: 'administrator@amptesting.com',
+        htmlContains: 'You will receive another email shortly to confirm your registration status'
+      });
+    });
+
     cy.contains('Registrations').click()
     cy.contains('Approve All').click()
     cy.wait(1000)
@@ -21,6 +36,19 @@ describe('Test the registration feature', () => {
     cy.contains('Cypress-example-event').click()
 
     cy.contains('Approved: Yes')
+
+    // Check for approved registration email
+    cy.waitForEmail({
+      to: 'administrator@amptesting.com',
+      subject: 'Registration Confirmed for cypress-example-event'
+    }).then((message) => {
+      cy.assertEmailContent(message, {
+        subject: 'Registration Confirmed for cypress-example-event',
+        from: 'noreply@support.access-ci.org',
+        to: 'administrator@amptesting.com',
+        htmlContains: 'Congratulations! Your registration for&nbsp;'
+      });
+    });
   })
 
   it('Walnut register for event', () => {
@@ -34,8 +62,23 @@ describe('Test the registration feature', () => {
     cy.contains('Please confirm your registration below')
     cy.get('#edit-submit').click()
 
-    cy.contains('You will receive an email after your registration is approved')
+    cy.contains('Successfully registered to the waitlist.')
     cy.contains('Approved: No')
+    cy.contains('Waitlist: Yes')
+
+    // Check for waitlist registration email
+    cy.waitForEmail({
+      to: 'walnut@pie.org',
+      subject: 'Waitlist Status for cypress-example-event'
+    }).then((message) => {
+      cy.assertEmailContent(message, {
+        subject: 'Waitlist Status for cypress-example-event',
+        from: 'noreply@support.access-ci.org',
+        to: 'walnut@pie.org',
+        htmlContains: 'At this time, the session is full, and your registration has been placed on the waitlist'
+      });
+    });
+
   })
 
   it('Approve Walnut registration as admin', () => {
@@ -59,6 +102,20 @@ describe('Test the registration feature', () => {
     cy.contains('cypress-example-event').click()
 
     cy.contains('Approved: Yes')
+
+    // Check for waitlist registration email
+    cy.waitForEmail({
+      to: 'walnut@pie.org',
+      subject: 'Registration Confirmed for cypress-example-event'
+    }).then((message) => {
+      cy.assertEmailContent(message, {
+        subject: 'Registration Confirmed for cypress-example-event',
+        from: 'noreply@support.access-ci.org',
+        to: 'walnut@pie.org',
+        htmlContains: 'has been confirmed'
+      });
+    });
+
   })
 
 })
