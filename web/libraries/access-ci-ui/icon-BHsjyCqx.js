dd704b331812335bfb655721d97570d91f44c9cf
Andrew Pasquale <andrew@elytra.net>
2025-08-25 15:23:44 -0400

    Use access-ci-ui v0.10.0

--- web/libraries/access-ci-ui/icon-BHsjyCqx.js
+++
@@ -1,7 +0,0 @@
-import { u as l } from "./index-GCiVlJeX.js";
-function u({ alt: n = null, name: i = null, src: o = null }) {
-  return i ? /* @__PURE__ */ l("i", { class: `icon bi bi-${i}` }) : /* @__PURE__ */ l("img", { class: "icon", alt: n, src: o });
-}
-export {
-  u as I
-};
