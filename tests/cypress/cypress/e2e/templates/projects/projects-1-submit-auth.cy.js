29f9b5ba34b0906de57755811bfe95dd4ef32f23
Andrew Pasquale <andrew@elytra.net>
2025-11-07 16:37:56 -0500

    Remove project description comment from test
    Removed a comment about providing project description.

--- tests/cypress/cypress/e2e/templates/projects/projects-1-submit-auth.cy.js
+++ tests/cypress/cypress/e2e/templates/projects/projects-1-submit-auth.cy.js
@@ -15,7 +15,6 @@ describe("test submit project form", () => {
     // cy.contains('Work Phone'); // Work phone field is hidden for non-administrators
     // cy.contains('Ext:'); // Work phone field is hidden for non-administrators
     cy.contains('Project Information');
-    cy.contains('Provide a description of the project and its history, progress, and/or current status.');
     cy.get('input[name="project_title"]').type('TEST', { delay: 0 });
     // cy.get('input[name="region[345]"]').check(); // At-Large
     cy.get('input[name="project_leader[first]"]').type('TEST', { delay: 0 });
