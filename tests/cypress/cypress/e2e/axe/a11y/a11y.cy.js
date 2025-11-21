83336c42d30f15bbf8fcc083c6edf21a1d2b8ef5
protitude <miles.france@gmail.com>
2025-11-21 08:14:22 -0700

    cypress: d8-2585
    refactor ally tests

--- tests/cypress/cypress/e2e/axe/a11y/a11y.cy.js
+++ tests/cypress/cypress/e2e/axe/a11y/a11y.cy.js
@@ -1,468 +1,150 @@
+// Helper function to check accessibility for a given URL
+const checkA11y = (url) => {
+  cy.visit(url);
+  cy.injectAxe();
+  cy.a11yCheckWithReport({
+    exclude: [
+      [
+        ['#universal-menus'],
+        ['#header'],
+        ['#site-menus']
+      ]
+    ]
+  });
+};
+
+// Helper function to check multiple URLs in a single test
+const checkMultipleUrls = (urls) => {
+  urls.forEach(url => checkA11y(url));
+};
+
 describe('Accessibility Testing - reports all violations without failing CI (initial phase)', () => {
   it('home', () => {
-    cy.visit('/');
-    cy.injectAxe();
-    cy.a11yCheckWithReport({
-      exclude: [
-        [
-          ['#universal-menus'],
-          ['#header'],
-          ['#site-menus']
-        ]
-      ]
-    });
+    checkA11y('/');
   });
 
   it('announcements', () => {
-    cy.visit('/announcements');
-    cy.injectAxe();
-    cy.a11yCheckWithReport({
-      exclude: [
-        [
-          ['#universal-menus'],
-          ['#header'],
-          ['#site-menus']
-        ]
-      ]
-    });
-
-    cy.visit('/announcements/tacc-status-saturday-september-27-2025');
-    cy.injectAxe();
-    cy.a11yCheckWithReport({
-      exclude: [
-        [
-          ['#universal-menus'],
-          ['#header'],
-          ['#site-menus']
-        ]
-      ]
-    });
+    checkMultipleUrls([
+      '/announcements',
+      '/announcements/tacc-status-saturday-september-27-2025'
+    ]);
   });
 
   it('events', () => {
-    cy.visit('/events');
-    cy.injectAxe();
-    cy.a11yCheckWithReport({
-      exclude: [
-        [
-          ['#universal-menus'],
-          ['#header'],
-          ['#site-menus']
-        ]
-      ]
-    });
-
-    cy.visit('/events/8482');
-    cy.injectAxe();
-    cy.a11yCheckWithReport({
-      exclude: [
-        [
-          ['#universal-menus'],
-          ['#header'],
-          ['#site-menus']
-        ]
-      ]
-    });
+    checkMultipleUrls([
+      '/events',
+      '/events/8482'
+    ]);
   });
 
   it('Outages', () => {
-    cy.visit('/outages');
-    cy.injectAxe();
-    cy.a11yCheckWithReport({
-      exclude: [
-        [
-          ['#universal-menus'],
-          ['#header'],
-          ['#site-menus']
-        ]
-      ]
-    });
+    checkA11y('/outages');
   });
 
   it('Community', () => {
-    cy.visit('/community/overview');
-    cy.injectAxe();
-    cy.a11yCheckWithReport({
-      exclude: [
-        [
-          ['#universal-menus'],
-          ['#header'],
-          ['#site-menus']
-        ]
-      ]
-    });
+    checkA11y('/community/overview');
   });
 
   it('Affinity Groups', () => {
-    cy.visit('/affinity-groups');
-    cy.injectAxe();
-    cy.a11yCheckWithReport({
-      exclude: [
-        [
-          ['#universal-menus'],
-          ['#header'],
-          ['#site-menus']
-        ]
-      ]
-    });
-
-    cy.visit('/affinity-groups/anvil');
-    cy.injectAxe();
-    cy.a11yCheckWithReport({
-      exclude: [
-        [
-          ['#universal-menus'],
-          ['#header'],
-          ['#site-menus']
-        ]
-      ]
-    });
+    checkMultipleUrls([
+      '/affinity-groups',
+      '/affinity-groups/anvil'
+    ]);
   });
 
   it('CSSN', () => {
-    cy.visit('/community/cssn');
-    cy.injectAxe();
-    cy.a11yCheckWithReport({
-      exclude: [
-        [
-          ['#universal-menus'],
-          ['#header'],
-          ['#site-menus']
-        ]
-      ]
-    });
-
-    cy.visit('/community/cssn/directory');
-    cy.injectAxe();
-    cy.a11yCheckWithReport({
-      exclude: [
-        [
-          ['#universal-menus'],
-          ['#header'],
-          ['#site-menus']
-        ]
-      ]
-    });
-
-    cy.visit('/community-persona/100');
-    cy.injectAxe();
-    cy.a11yCheckWithReport({
-      exclude: [
-        [
-          ['#universal-menus'],
-          ['#header'],
-          ['#site-menus']
-        ]
-      ]
-    });
-
-    cy.visit('/community/badges');
-    cy.injectAxe();
-    cy.a11yCheckWithReport({
-      exclude: [
-        [
-          ['#universal-menus'],
-          ['#header'],
-          ['#site-menus']
-        ]
-      ]
-    });
-
-    cy.visit('/community/scipe');
-    cy.injectAxe();
-    cy.a11yCheckWithReport({
-      exclude: [
-        [
-          ['#universal-menus'],
-          ['#header'],
-          ['#site-menus']
-        ]
-      ]
-    });
+    checkMultipleUrls([
+      '/community/cssn',
+      '/community/cssn/directory',
+      '/community-persona/100',
+      '/community/badges',
+      '/community/scipe'
+    ]);
   });
 
-  it('Workforce Dev Workshops ', () => {
-    cy.visit('/ccep-workforce-development-funded-workshops');
-    cy.injectAxe();
-    cy.a11yCheckWithReport({
-      exclude: [
-        [
-          ['#universal-menus'],
-          ['#header'],
-          ['#site-menus']
-        ]
-      ]
-    });
+  it('Workforce Dev Workshops', () => {
+    checkA11y('/ccep-workforce-development-funded-workshops');
   });
 
-  it('Knowledge Base Overview ', () => {
-    cy.visit('/knowledge-base/overview');
-    cy.injectAxe();
-    cy.a11yCheckWithReport({
-      exclude: [
-        [
-          ['#universal-menus'],
-          ['#header'],
-          ['#site-menus']
-        ]
-      ]
-    });
-
-    cy.visit('/knowledge-base/resources');
-    cy.injectAxe();
-    cy.a11yCheckWithReport({
-      exclude: [
-        [
-          ['#universal-menus'],
-          ['#header'],
-          ['#site-menus']
-        ]
-      ]
-    });
+  it('Knowledge Base Overview', () => {
+    checkMultipleUrls([
+      '/knowledge-base/overview',
+      '/knowledge-base/resources'
+    ]);
   });
 
   it('Find', () => {
-    cy.visit('/find');
-    cy.injectAxe();
-    cy.a11yCheckWithReport({
-      exclude: [
-        [
-          ['#universal-menus'],
-          ['#header'],
-          ['#site-menus']
-        ]
-      ]
-    });
+    checkA11y('/find');
   });
 
   it('Tags', () => {
-    cy.visit('/tags');
-    cy.injectAxe();
-    cy.a11yCheckWithReport({
-      exclude: [
-        [
-          ['#universal-menus'],
-          ['#header'],
-          ['#site-menus']
-        ]
-      ]
-    });
+    checkA11y('/tags');
   });
 
   it('Video Learning Center', () => {
-    cy.visit('/video-learning-center');
-    cy.injectAxe();
-    cy.a11yCheckWithReport({
-      exclude: [
-        [
-          ['#universal-menus'],
-          ['#header'],
-          ['#site-menus']
-        ]
-      ]
-    });
+    checkA11y('/video-learning-center');
   });
 
   it('MATCH Services', () => {
-    cy.visit('/match/overview');
-    cy.injectAxe();
-    cy.a11yCheckWithReport({
-      exclude: [
-        [
-          ['#universal-menus'],
-          ['#header'],
-          ['#site-menus']
-        ]
-      ]
-    });
+    checkA11y('/match/overview');
   });
 
   it('ACCESS and NAIRR Pilot Office Hours', () => {
-    cy.visit('/access-and-nairr-pilot-office-hours');
-    cy.injectAxe();
-    cy.a11yCheckWithReport({
-      exclude: [
-        [
-          ['#universal-menus'],
-          ['#header'],
-          ['#site-menus']
-        ]
-      ]
-    });
+    checkA11y('/access-and-nairr-pilot-office-hours');
   });
 
   it('ACCESS Tools', () => {
-    cy.visit('/tools/overview');
-    cy.injectAxe();
-    cy.a11yCheckWithReport({
-      exclude: [
-        [
-          ['#universal-menus'],
-          ['#header'],
-          ['#site-menus']
-        ]
-      ]
-    });
+    checkA11y('/tools/overview');
   });
 
   it('OnDemand', () => {
-    cy.visit('/tools/ondemand');
-    cy.injectAxe();
-    cy.a11yCheckWithReport({
-      exclude: [
-        [
-          ['#universal-menus'],
-          ['#header'],
-          ['#site-menus']
-        ]
-      ]
-    });
+    checkA11y('/tools/ondemand');
   });
 
   it('Pegasus Workflows', () => {
-    cy.visit('/tools/pegasus');
-    cy.injectAxe();
-    cy.a11yCheckWithReport({
-      exclude: [
-        [
-          ['#universal-menus'],
-          ['#header'],
-          ['#site-menus']
-        ]
-      ]
-    });
+    checkA11y('/tools/pegasus');
   });
 
   it('Science Gateways', () => {
-    cy.visit('/tools/science-gateways');
-    cy.injectAxe();
-    cy.a11yCheckWithReport({
-      exclude: [
-        [
-          ['#universal-menus'],
-          ['#header'],
-          ['#site-menus']
-        ]
-      ]
-    });
+    checkA11y('/tools/science-gateways');
   });
 
   it('XDMoD', () => {
-    cy.visit('/tools/xdmod');
-    cy.injectAxe();
-    cy.a11yCheckWithReport({
-      exclude: [
-        [
-          ['#universal-menus'],
-          ['#header'],
-          ['#site-menus']
-        ]
-      ]
-    });
+    checkA11y('/tools/xdmod');
   });
 
   it('open a help ticket', () => {
-    cy.visit('/help-ticket');
-    cy.injectAxe();
-    cy.a11yCheckWithReport({
-      exclude: [
-        [
-          ['#universal-menus'],
-          ['#header'],
-          ['#site-menus']
-        ]
-      ]
-    });
+    checkA11y('/help-ticket');
   });
 
   it('create an announcement', () => {
     cy.loginAs('pecan@pie.org', 'Pecan');
-    cy.visit('/node/add/access_news');
-    cy.injectAxe();
-    cy.a11yCheckWithReport({
-      exclude: [
-        [
-          ['#universal-menus'],
-          ['#header'],
-          ['#site-menus']
-        ]
-      ]
-    });
+    checkA11y('/node/add/access_news');
   });
 
   it('create an event', () => {
     cy.loginAs('pecan@pie.org', 'Pecan');
-    cy.visit('/events/add');
-    cy.injectAxe();
-    cy.a11yCheckWithReport({
-      exclude: [
-        [
-          ['#universal-menus'],
-          ['#header'],
-          ['#site-menus']
-        ]
-      ]
-    });
+    checkA11y('/events/add');
   });
 
   it('request an affinity group', () => {
     cy.loginAs('pecan@pie.org', 'Pecan');
-    cy.visit('/events/add/default');
-    cy.injectAxe();
-    cy.a11yCheckWithReport({
-      exclude: [
-        [
-          ['#universal-menus'],
-          ['#header'],
-          ['#site-menus']
-        ]
-      ]
-    });
+    checkA11y('/events/add/default');
   });
 
   it('add a KB resource', () => {
     cy.loginAs('pecan@pie.org', 'Pecan');
-    cy.visit('/form/resource');
-    cy.injectAxe();
-    cy.a11yCheckWithReport({
-      exclude: [
-        [
-          ['#universal-menus'],
-          ['#header'],
-          ['#site-menus']
-        ]
-      ]
-    });
+    checkA11y('/form/resource');
   });
 
   it('request an engagement', () => {
     cy.loginAs('pecan@pie.org', 'Pecan');
-    cy.visit('/node/add/match_engagement?type=plus');
-    cy.injectAxe();
-    cy.a11yCheckWithReport({
-      exclude: [
-        [
-          ['#universal-menus'],
-          ['#header'],
-          ['#site-menus']
-        ]
-      ]
-    });
+    checkA11y('/node/add/match_engagement?type=plus');
   });
 
   it('Edit Community Persona', () => {
     cy.loginAs('pecan@pie.org', 'Pecan');
-    cy.visit('/user/edit');
-    cy.injectAxe();
-    cy.a11yCheckWithReport({
-      exclude: [
-        [
-          ['#universal-menus'],
-          ['#header'],
-          ['#site-menus']
-        ]
-      ]
-    });
+    checkA11y('/user/edit');
   });
 
 });
